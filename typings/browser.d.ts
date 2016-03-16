b153a1bf547264fccbb2d23c20f5e12751422019
Ronan Brett <Ronan.Brett@theaa.ie>
2016-03-16 17:22:36 +0000

    Remove Webpack from Typings

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,3 +1,2 @@
 /// <reference path="browser/ambient/node/index.d.ts" />
 /// <reference path="browser/ambient/react/index.d.ts" />
-/// <reference path="browser/definitions/webpack/index.d.ts" />
--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,3 +1,2 @@
 /// <reference path="main/ambient/node/index.d.ts" />
 /// <reference path="main/ambient/react/index.d.ts" />
-/// <reference path="main/definitions/webpack/index.d.ts" />
